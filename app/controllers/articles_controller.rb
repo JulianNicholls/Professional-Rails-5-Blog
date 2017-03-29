fe44087c6e6a2cca271873390f8a8da9fe99f27b
Julian Nicholls <juliannicholls29@gmail.com>
2017-03-29 15:10:27 +0100

    Add edit action and more links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new article_params
+
     if @article.save
       redirect_to @article
     else
@@ -27,11 +28,17 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find params[:id]
+
+    if @article.update article_params
+      redirect_to @article
+    else
+      render :edit
+    end
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit :title, :text
   end
 end
